dac0254a3d863c79b0caf05df89a289a9a40d96a
Nidhi Chaurasia <ch.nidhi99@gmail.com>
2024-08-10 22:40:07 +0530

    New program

---
+++ python_lc.py
@@ -0,0 +1,7 @@
+# Define the list to of numbers
+numbers = [1, 2, 3, 4, 5, 6]
+
+#Iterate through the list and print even numbers only
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
